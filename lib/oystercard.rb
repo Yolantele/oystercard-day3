93c6bde823f8a488e2a4781198b5972b3ab2665c
Robert Clayton <robbie.c.clayton@gmail.com>
2017-11-01 12:16:13 +0000

    Updated Syntax

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,58 +1,41 @@
+# This is a simulation of an Oystercard system
 class Oystercard
+  LIMIT = 90
+  MINIMUM_FARE = 1
 
-describe Oystercard do
-  subject(:oystercard) { described_class.new }
+  attr_reader :balance, :state
 
-  describe 'initialize' do
-    it 'Check if oystercard has a balance equal to 0' do
-      expect(subject.balance).to eq(0)
-    end
+  def initialize(balance = 0)
+    @balance = balance
+    @state = :not_in_use
   end
 
-  describe '#top_up' do
-    it { is_expected.to respond_to(:top_up) }
-    it 'Check if @balance increase after top_up' do
-      subject.top_up(5)
-      expect(subject.balance).to eq(5)
-    end
-    it 'top_up raise error when above the limit' do
-      limit = Oystercard::LIMIT - subject.balance
-      money_to_put = limit + 1
-      error_top_up = "You are exceeding the limit in your card. You can top_up only #{limit}"
-      expect { subject.top_up(money_to_put) }.to raise_error(error_top_up)
-    end
+  def top_up(amount)
+    raise error_top_up if @balance + amount > LIMIT
+    @balance += amount
   end
-  # describe '#deduct' do
-    # it 'reduces the balance by certain amount' do
-      # subject.top_up(50)
-      # expect(subject.deduct(25)).to eq(25)
-    # end
-  # end
-  describe '#touch_in' do
-    it 'set the card as in use' do
-      allow(subject).to receive(:touch_in).and_return(:in_use)
-      expect(subject.touch_in).to eq(:in_use)
-    end
-    it 'raises error if balance is less than minimum' do
-      expect { subject.touch_in }.to raise_error('Insufficient balance for travel')
-    end
+
+  def touch_in
+    raise 'Insufficient balance for travel' if @balance < MINIMUM_FARE
+    @state = :in_use
   end
-  describe '#touch_out' do
-    it 'set the card as not in used' do
-      expect(subject.touch_out).to eq(:not_in_use)
-    end
-    it 'check if touch_out reduce balance by minumum fare' do
-      minimum_fare = Oystercard::MINIMUM_FARE
-      subject.top_up(1)
-      subject.touch_in
-      expect { subject.touch_out }.to change { subject.balance }.by(minimum_fare * -1)
-    end
+
+  def touch_out
+    deduct if @state == :in_use
+    @state = :not_in_use
   end
-  describe '#in_journey?' do
-    it 'Check if the card is in use or not.' do
-      subject.top_up(2)
-      subject.touch_in
-      expect(subject.in_journey?).to eq(true)
-    end
+
+  def in_journey?
+    @state == :in_use
+  end
+
+  def error_top_up
+    "This exceeds the card limit. You can top_up only #{LIMIT - @balance}"
+  end
+
+  private
+
+  def deduct
+    @balance -= MINIMUM_FARE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,41 +1,65 @@
 require './lib/oystercard.rb'
 
-class Oystercard
-LIMIT = 90
-MINIMUM_FARE = 1
-  attr_reader :balance
+describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  let(:topped_up_card) { described_class.new }
 
-  def initialize(balance = 0)
-    @balance = balance
-    @state = :not_in_use
+  before do
+    topped_up_card.top_up(5)
   end
 
-  def top_up(amount)
-    raise error_top_up if @balance + amount > LIMIT
-    @balance += amount
+  describe 'initialize' do
+    it 'Check if oystercard has a balance equal to 0' do
+      expect(subject.balance).to eq(0)
+    end
   end
 
-  def error_top_up
-    "You are exceeding the limit in your card. You can top_up only #{LIMIT - @balance}"
+  describe '#top_up' do
+    it 'Check if @balance increase after top_up' do
+      expect(topped_up_card.balance).to eq(5)
+    end
+
+    it 'top_up raise error when above the limit' do
+      subject.top_up(Oystercard::LIMIT)
+      sum = Oystercard::LIMIT - subject.balance
+      error_top_up = "This exceeds the card limit. You can top_up only #{sum}"
+      expect { subject.top_up(1) }.to raise_error(error_top_up)
+    end
   end
 
-  def touch_in
-    raise 'Insufficient balance for travel' if @balance < MINIMUM_FARE
-    @state = :in_use
+  describe '#touch_in' do
+    it 'should set the card to in use' do
+      topped_up_card.touch_in
+      expect(topped_up_card.state).to eq(:in_use)
+    end
+
+    it 'raises error if balance is less than minimum' do
+      message = 'Insufficient balance for travel'
+      expect { subject.touch_in }.to raise_error(message)
+    end
   end
 
-  def touch_out
-    deduct if @state == :in_use
-    @state = :not_in_use
+  describe '#touch_out' do
+    def in_out
+      topped_up_card.touch_in
+      topped_up_card.touch_out
+    end
+
+    it 'should change state to not in use' do
+      in_out
+      expect(topped_up_card.state).to eq(:not_in_use)
+    end
+
+    it 'check if touch_out reduce balance by minumum fare' do
+      in_out
+      expect(topped_up_card.balance).to eq(4)
+    end
   end
 
-  def in_journey?
-    @state == :in_use
-  end
-
-  private
-
-  def deduct
-    @balance -= MINIMUM_FARE
+  describe '#in_journey?' do
+    it 'Check if the card is in use or not.' do
+      topped_up_card.touch_in
+      expect(topped_up_card.in_journey?).to eq(true)
+    end
   end
 end
