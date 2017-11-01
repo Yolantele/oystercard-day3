3b2333dde9d442d05d4fc8272f76ac363b1cf2f5
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-01 15:30:41 +0000

    Created a station class and Station spec file

---
+++ spec/station_spec.rb
@@ -0,0 +1,16 @@
+require './lib/station.rb'
+
+describe Station do
+
+  subject(:station) { described_class.new('abc', 1) }
+
+  describe 'initialize' do
+    it 'should return station\'s name' do
+      expect(subject.name).to eq 'abc'
+    end
+
+    it 'should return station\'s zone' do
+      expect(subject.zone).to eq 1
+    end
+  end
+end
